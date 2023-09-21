7a233b342819f7dd2ad5cbb8307fb4a86357a05b
diegohansselperez <diegohansselp24@gmail.com>
2023-09-21 00:14:10 -0700

    mostramos los libros en el sitio web del cliente

--- productos.php
+++ productos.php
@@ -1,33 +1,21 @@
 <?php include("./template/header.php") ?>
+<?php include("administrador/config/database.php");
+    $sentenciaSQL = $conexion->prepare("SELECT * FROM libros2");
+    $sentenciaSQL->execute();
+    $listaLibros = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+    
+?>
 <div class="row gap-1 justify-content-center m-0 p-0">
-    <div class="card col-3">
-        <img class="card-img-top rounded-3" src="https://images.pexels.com/photos/25758/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+    <?php foreach($listaLibros as $libro){ ?>    
+<div class="card col-3 py-2">
+
+        <img class="card-img-top rounded-3 border" src="/img/<?php echo $libro["imagen"] ;?>" alt="">
         <div class="card-body ">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+            <h4 class="card-title"><?php echo $libro['nombre']?></h4>
+            <a name="" id="" class="btn btn-primary" target="_blank" href="https://books.goalkicker.com/" role="button">Ver mas</a>
         </div>
     </div>
-    <div class="card col-3">
-        <img class="card-img-top" src="https://images.pexels.com/photos/25758/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-        </div>
-    </div>
-    <div class="card col-3">
-        <img class="card-img-top" src="https://images.pexels.com/photos/25758/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-        </div>
-    </div>
-    <div class="card col-3">
-        <img class="card-img-top" src="https://images.pexels.com/photos/25758/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
-        <div class="card-body">
-            <h4 class="card-title">Title</h4>
-            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
-        </div>
-    </div>
+    <?php }?>
     
 </div>
 
