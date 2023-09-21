d2f542462a8e530cfe49c2af3510ba35eebbeb5d
diegohansselperez <diegohansselp24@gmail.com>
2023-09-21 10:00:58 -0700

    Rediseñamos el inicio de la pagina principal

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include("./template/header.php") ?>
-<div class="jumbotron">
-    <h1 class="display-3">Jumbo heading</h1>
-    <p class="lead">Jumbo helper text</p>
+<div class="jumbotron text-center">
+    <h1 class="display-3">Bienvenidos a National Library</h1>
+    <p class="lead">Encontraras cualquier libro de programacion para todos los lenguajes.</p>
     <hr class="my-2">
-    <p>More info</p>
+    <img width="400" src="/img/stock_women.jpg" class="img-thumbnail rounded mx-auto d-block" alt="">
+    <p>Mas informacion</p>
     <p class="lead">
-        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver almacen de libros</a>
     </p>
 </div>
 <?php include("./template/footer.php") ?>
